6373debda6ecde86a8e7a7cd6c7eccb57f647b36
catherine <zhangjiecatherine@gmail.com>
2019-09-15 21:35:28 -0400

    Added 3 layouts for Lab2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -9,6 +9,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        //view the Linear page
+        //setContentView(R.layout.activity_main_linear);
+
+        //view the grid page
+        //setContentView(R.layout.activity_main_grid);
+
+        //view the relative page
+        setContentView(R.layout.activity_main_relative);
     }
 }
