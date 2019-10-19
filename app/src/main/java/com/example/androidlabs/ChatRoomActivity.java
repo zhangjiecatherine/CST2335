558c00e07e9564cb321604d3960d9253fe3e5bc5
catherine <zhangjiecatherine@gmail.com>
2019-10-18 22:41:30 -0400

    Lab5-Add Database

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -1,229 +1,154 @@
 package com.example.androidlabs;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.EditText;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
+import static com.example.androidlabs.MyDatabaseOpenHelper.*;
 
 public class ChatRoomActivity extends AppCompatActivity {
 
-    int numObjects = 6;
+    ArrayList<Message> objects = new ArrayList<>();
+
+    BaseAdapter myAdapter;
+    SQLiteDatabase db;
+    Cursor result;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab4_chat);
 
-        //ListAdapter adt = new MyArrayAdapter(new String[] {"A", "B", "C"});
-        ListAdapter adt = new MyOwnAdapter();
-        ChatAdapter adapter = new ChatAdapter();
-
         ListView theList = findViewById(R.id.the_list);
-        SwipeRefreshLayout refresher = findViewById(R.id.refresher) ;
-        refresher.setOnRefreshListener(()-> {
-            numObjects *= 2;
-            ((MyOwnAdapter) adt).notifyDataSetChanged();
-            refresher.setRefreshing( false );
+        theList.setAdapter( myAdapter = new MyListAdapter() );
+        theList.setOnItemClickListener( ( parent,  view,  position,  id) ->{
+            Log.i("CLicked", "You clicked item:" + position);
         });
+//get a database
+        MyDatabaseOpenHelper dbOpener= new MyDatabaseOpenHelper(this);
+        db = dbOpener.getWritableDatabase();
+        //db.execSQL("delete from "+ TABLE_NAME);
+        //query all the results from the database
+        String[] colums = {COL_ID, COL_MESSAGE, COL_ISSENT};
+        result = db.query(false, TABLE_NAME, colums,
+                null,null,null, null,null,null);
 
-        //theList.setAdapter(adt);
+        //find the column index
+        int nameColIndex = result.getColumnIndex(COL_MESSAGE);
+        int idConInex = result.getColumnIndex(COL_ID);
+        int isSentConIndex = result.getColumnIndex(COL_ISSENT);
 
-        //This listens for items being clicked in the list view
-        theList.setOnItemClickListener(( parent,  view,  position,  id) -> {
-            Log.e("you clicked on :" , "item "+ position);
-            numObjects = 20;
-            ((MyOwnAdapter) adt).notifyDataSetChanged();
-        });
+        //iterate over the results, return true if there is a next item;
+        while (result.moveToNext()){
+            String name = result.getString(nameColIndex);
+            long id = result.getLong(idConInex);
+            boolean isSent = result.getInt(isSentConIndex)>0;
+            objects.add(new Message(name, isSent, id));
+        }
+        Button receiveButton = findViewById(R.id.receiveButton);
+        receiveButton.setOnClickListener( clik -> {
+            TextView msgText = findViewById(R.id.userTyped);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COL_MESSAGE , msgText.getText().toString());
+            contentValues.put(COL_ISSENT, true);
+            long newId = db.insert(TABLE_NAME,null,contentValues);
+            Message message = new Message(msgText.getText().toString(),true, newId);
+            this.printCursor();
+            objects.add(message);
+            myAdapter.notifyDataSetChanged(); //update yourself
+            msgText.setText("");
+        } );
+
+        Button sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener( clik -> {
+            TextView msgText = findViewById(R.id.userTyped);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COL_MESSAGE , msgText.getText().toString());
+            contentValues.put(COL_ISSENT, false);
+            long newId = db.insert(TABLE_NAME,null,contentValues);
+            Message message = new Message(msgText.getText().toString(),false,newId);
+            this.printCursor();
+            objects.add(message);
+            myAdapter.notifyDataSetChanged(); //update yourself
+            msgText.setText("");
+        } );
 
 
-        Button sendButton = (Button)findViewById(R.id.sendButton);
-        sendButton.setOnClickListener( c -> {
-            EditText userTyped = (EditText)findViewById(R.id.userTyped);
-            String userType = userTyped.getText().toString();
-            userTyped.setText("");
-            adapter.messages.add(new Message(userType,1));
-            adapter.notifyDataSetChanged();
-        });
-
-        Button receiveButton = (Button)findViewById(R.id.receiveButton);
-        receiveButton.setOnClickListener( c -> {
-            EditText userTyped = (EditText)findViewById(R.id.userTyped);
-            String userType = userTyped.getText().toString();
-            userTyped.setText("");
-            adapter.messages.add(new Message(userType,2));
-            adapter.notifyDataSetChanged();
-        });
-
-        theList.setAdapter(adapter);
     }
 
-    //This class needs 4 functions to work properly:
-    protected class ChatAdapter extends BaseAdapter
-    {
-        List<Message> messages = new ArrayList<>();
-        @Override
-        public int getCount() {
-            return messages.size();
-        }
+    private class MyListAdapter extends BaseAdapter {
 
-        public Object getItem(int position){
-            return messages.get(position).getMessage();//"\nItem "+ (position+1) + "\nSub Item "+ (position+1) +"\n";
-        }
+        public int getCount() {  return objects.size();  } //This function tells how many objects to show
 
-        public View getView(int position, View old, ViewGroup parent)
+        public Message getItem(int position) { return objects.get(position);  }  //This returns the string at position p
+
+        public long getItemId(int p) { return p; } //This returns the database id of the item at position p
+
+        public View getView(int p, View recycled, ViewGroup parent)
         {
+
             LayoutInflater inflater = getLayoutInflater();
-            int type = messages.get(position).getType();
-            View newView;
-            if(type ==1){
-                newView = inflater.inflate(R.layout.listview_item_receive, parent, false );
-            }else{
-                newView = inflater.inflate(R.layout.listview_item_send, parent, false );
+            View thisRow = recycled;
+
+            if(thisRow == null) {
+                if (getItem(p).isSent) {
+                    thisRow = inflater.inflate(R.layout.listview_item_receive, null);
+                    TextView itemField = thisRow.findViewById(R.id.edit);
+                    itemField.setText(getItem(p).message);
+                } else {
+                    thisRow = inflater.inflate(R.layout.listview_item_send, null);
+                    TextView itemField = thisRow.findViewById(R.id.edit);
+                    itemField.setText(getItem(p).message);
+                }
+            } else {
+                if (getItem(p).isSent) {
+                    thisRow = inflater.inflate(R.layout.listview_item_receive, null);
+                    TextView itemField = thisRow.findViewById(R.id.edit);
+                    itemField.setText(getItem(p).message);
+                } else {
+                    thisRow = inflater.inflate(R.layout.listview_item_send, null);
+                    TextView itemField = thisRow.findViewById(R.id.edit);
+                    itemField.setText(getItem(p).message);
+                }
             }
-            TextView rowText = (TextView)newView.findViewById(R.id.edit);
-            String stringToShow = getItem(position).toString();
-            rowText.setText( stringToShow );
-            return newView;
-        }
-
-        public long getItemId(int position)
-        {
-            return position;
+            return thisRow;
         }
     }
 
+    public void printCursor() {
+        Log.e("MyDatabaseFile version:", db.getVersion() + "");
+        Log.e("Number of columns:", result.getColumnCount() + "");
+        Log.e("Name of the columns:", Arrays.toString(result.getColumnNames()));
+        Log.e("Number of results", result.getCount() + "");
+        Log.e("Each row of results :", "");
+        result.moveToFirst();
 
+        //find the column index
 
+        for (int i = 0; i < result.getCount(); i++) {
+            while (!result.isAfterLast()) {
 
+                Log.e("id", result.getString(result.getColumnIndex(COL_ID)) + "");
+                Log.e("isSent", result.getString(result.getColumnIndex(COL_ISSENT)) + "");
+                Log.e("message", result.getString(result.getColumnIndex(COL_MESSAGE)) + "");
 
-    //This class needs 4 functions to work properly:
-    protected class MyOwnAdapter extends BaseAdapter
-    {
-
-        @Override
-        public int getCount() {
-            return numObjects;
-        }
-
-        public Object getItem(int position){
-            return "\nItem "+ (position+1) + "\nSub Item "+ (position+1) +"\n";
-        }
-
-        public View getView(int position, View old, ViewGroup parent)
-        {
-            LayoutInflater inflater = getLayoutInflater();
-
-            //View newView = inflater.inflate(R.layout.activity_lab4_chat, parent, false );
-            View newView = inflater.inflate(R.layout.activity_lab4_chat, parent, false );
-
-            TextView rowText = (TextView)newView.findViewById(R.id.userTyped);
-            String stringToShow = getItem(position).toString();
-            rowText.setText( stringToShow );
-            //return the row:
-            return newView;
-        }
-
-        public long getItemId(int position)
-        {
-            return position;
-        }
-    }
-
-    //A copy of ArrayAdapter. You just give it an array and it will do the rest of the work.
-    protected class MyArrayAdapter<E> extends BaseAdapter
-    {
-        private List<E> dataCopy = null;
-
-        //Keep a reference to the data:
-        public MyArrayAdapter(List<E> originalData)
-        {
-            dataCopy = originalData;
-        }
-
-        //You can give it an array
-        public MyArrayAdapter(E [] array)
-        {
-            dataCopy = Arrays.asList(array);
-        }
-
-
-        //Tells the list how many elements to display:
-        public int getCount()
-        {
-            return dataCopy.size();
-        }
-
-
-        public E getItem(int position){
-            return dataCopy.get(position);
-        }
-
-        public View getView(int position, View old, ViewGroup parent)
-        {
-            //get an object to load a layout:
-            LayoutInflater inflater = getLayoutInflater();
-
-            //Recycle views if possible:
-            TextView root = (TextView)old;
-            //If there are no spare layouts, load a new one:
-            if(old == null)
-                root = (TextView)inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
-
-            //Get the string to go in row: position
-            String toDisplay = getItem(position).toString();
-
-            //Set the text of the text view
-            root.setText(toDisplay);
-
-            //Return the text view:
-            return root;
-        }
-
-
-        //Return 0 for now. We will change this when using databases
-        public long getItemId(int position)
-        {
-            return 0;
-        }
-    }
-
-    //Message class
-    protected class Message {
-        String message;
-        int type;
-
-        public Message(){
-        }
-        public Message(String message,int type){
-            this.message = message;
-            this.type = type;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-        public String getMessage(){
-            return this.message;
-        }
-        public void setType(int type) {
-            this.type = type;
-        }
-        public int getType(){
-            return this.type;
+                result.moveToNext();
+            }
         }
     }
 }
